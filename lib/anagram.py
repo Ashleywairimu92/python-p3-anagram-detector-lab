acbfb485cbf3f9d1745795746ddbbb4f55e10e76
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-08-31 22:41:46 +0300

    updates

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        """Returns a list of words that are anagrams of the initialized word."""
+        def sorted_word(word):
+            return ''.join(sorted(word))
+
+        sorted_word_to_match = sorted_word(self.word)
+        return [word for word in words if sorted_word(word) == sorted_word_to_match]
+
